082f05db4a7373da500f2f53317df8ea3ae6f426
Wil Thomason <wil.thomason@gmail.com>
2015-03-15 20:06:43 -0400

    Changed scoring function to reflect desired ordering

--- negotiator_framework.py
+++ negotiator_framework.py
@@ -6,12 +6,8 @@ from negotiator import Negotiator
 def read_scenario(parameterfile_name):
     with open(parameterfile_name, 'r') as parameterfile:
         number_iterations = parameterfile.readline()
-        thresholds_a = islice(parameterfile, 2)
-        thresholds_b = islice(parameterfile, 2)
         return (
                 int(number_iterations),
-                tuple(map(int, thresholds_a)), 
-                tuple(map(int, thresholds_b)),
                 list(DictReader(parameterfile, fieldnames=["item_name", "negotiator_a", "negotiator_b"]))
                 )
 
@@ -31,15 +27,20 @@ if __id__ == "__main__":
     negotiator_a = Negotiator()
     negotiator_b = Negotiator()
     for scenario in argv:
-       (num_iters, thresh_a, thresh_b, mapping) = read_scenario(scenario)
-       negotiator_a.initialize({item["item_name"] : item["negotiator_a"] for item in mapping})
-       negotiator_b.initialize({item["item_name"] : item["negotiator_b"] for item in mapping})
+       (num_iters, mapping) = read_scenario(scenario)
+       a_mapping = {item["item_name"] : item["negotiator_a"] for item in mapping}
+       a_order = sorted(a_mapping, key=a_mapping.get, reverse=True)
+       b_mapping = {item["item_name"] : item["negotiator_b"] for item in mapping}
+       b_order = sorted(b_mapping, key=b_mapping.get, reverse=True)
+       negotiator_a.initialize(a_mapping)
+       negotiator_b.initialize(b_mapping)
        (result, count) = negotiate(num_iters, negotiator_a, negotiator_b)
-       points_a = points_b = 0
-       if result:
-           points_a = num_iters - count if thresh_a[0] <= count <= thresh_a[1] else 0
-           points_b = num_iters - count if thresh_b[0] <= count <= thresh_b[1] else 0
+       total = len(a_order)
+       points_a = reduce(lambda points, item: points + ((total / (a_order.index(item) + 1)) - abs(a_order.index(item) - result.index(item))), 0)
+       points_b = reduce(lambda points, item: points + ((total / (b_order.index(item) + 1)) - abs(b_order.index(item) - result.index(item))), 0)
        results = (result, points_a, points_b, count)
+       score_a += points_a
+       score_b += points_b
        negotiator_a.receive_results(results)
        negotiator_b.receive_results(results)
     print("Negotiator A: {}\nNegotiator B:{}".format(score_a, score_b))
