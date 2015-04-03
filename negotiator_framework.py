e7c502859e6db92d6ba69f884aeff423824be5ec
Wil Thomason <wil.thomason@gmail.com>
2015-04-02 23:54:52 -0400

    This is why static typing is good

--- negotiator_framework.py
+++ negotiator_framework.py
@@ -60,10 +60,12 @@ if __name__ == "__main__":
         (num_iters, mapping) = read_scenario(scenario)
         # Separate the mapping out for each negotiator, and sort the items from it into a list
         # based upon the preferences of each negotiator
-        a_mapping = {item["item_name"] : item["negotiator_a"] for item in mapping}
+        a_mapping = {item["item_name"] : int(item["negotiator_a"]) for item in mapping}
         a_order = sorted(a_mapping, key=a_mapping.get, reverse=True)
-        b_mapping = {item["item_name"] : item["negotiator_b"] for item in mapping}
+        b_mapping = {item["item_name"] : int(item["negotiator_b"]) for item in mapping}
         b_order = sorted(b_mapping, key=b_mapping.get, reverse=True)
+        print("B:")
+        print(b_order)
         # Give each negotiator their preferred item ordering
         negotiator_a.initialize(a_order, num_iters)
         negotiator_b.initialize(b_order, num_iters)
